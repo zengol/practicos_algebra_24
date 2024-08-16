dd5054b41d163ce115efb2fc6bcd251b9afb670c
zengolutil <zengolutil@gmail.com>
2024-08-16 17:09:55 -0300

    agregue los horarios

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!(
+        
+    "Buenas tardes!!! Soy Ariadna Nibeyro, profe del práctico de AGA. Nos veremos los viernes 16.30 o cuando finalice la clase teórica, por Zoom. Les dejo aquí el link para conectarse y el ID.
+
+Entrar Zoom Reunión
+https://us02web.zoom.us/j/6881123285?omn=85013767341
+
+ID de reunión: 688 112 3285"
+);
 }
