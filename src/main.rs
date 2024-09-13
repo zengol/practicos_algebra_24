33c94c1294f8d270faf00704b4e5168d921b4cb3
zengol <62455554+zengol@users.noreply.github.com>
2024-09-13 16:06:10 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!(
         
-    "Buenas tardes!!! Soy Ariadna Nibeyro, profe del práctico de AGA. Nos veremos los viernes 16.30 o cuando finalice la clase teórica, por Zoom. Les dejo aquí el link para conectarse y el ID.
+Les dejo aquí el link para conectarse y el ID.
 
 Entrar Zoom Reunión
 https://us02web.zoom.us/j/6881123285?omn=85013767341
